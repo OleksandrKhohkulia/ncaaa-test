9b286c73dd9f41157beb4330d1150f6a74c3eea1
Khokhulia <a.Oleksandr.Khokhuli@Nebraska.gov>
2025-05-02 09:01:52 -0500

    Meetings page : JS test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -132,7 +132,7 @@ console.log('page loaded')
 document.addEventListener('DOMContentLoaded',()=>{
   console.log(document.body)
   const meetingsBlock = document.querySelector(".meetings-form")
-  const meetingsForm = meetingsBlock.querySelector("form")
+  const meetingsForm = meetingsBlock.querySelector("div>div>p>*")
   console.log(meetingsForm)
   if(meetingsForm){
     const select = meetingsForm.Meetings
